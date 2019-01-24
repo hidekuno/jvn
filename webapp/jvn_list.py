e82c101fe8170659076713ef16bf6af33bc24217
hideki kuno <kunohi@firstserver.co.jp>
2019-01-24 15:45:41 +0900

    キーワード検索に対応した

--- webapp/jvn_list.py
+++ webapp/jvn_list.py
@@ -7,6 +7,7 @@
 #
 import os
 import jvn_pagination
+import logging
 from jvn_pagination import PAGE_COUNT
 from jvn_pagination import JvnPage
 ################################################################################
@@ -24,16 +25,24 @@ class JvnDAO(object):
             self.dp_from  = app.ui.dp_from
             self.dp_to    = app.ui.dp_to
 
+        self.keyword = app.ui.keyword
+
     def get_count(self):
         sql_count = """select count(*) from jvn_vulnerability a
                        where exists (select 1 from jvn_vulnerability_detail b, jvn_product c
                                      where a.identifier = b.identifier and b.cpe = c.cpe)"""
-        if self.dp_from == '' :
-            self.app.cursor.execute(sql_count)
-        else: 
-            sql_count = sql_count + """ and %s <= modified_date and modified_date <= %s;"""
-            self.app.cursor.execute(sql_count, (self.dp_from, self.dp_to))
+        params = list()
+        if self.keyword != '' :
+            params.append(self.make_like(self.keyword))
+            sql_count = sql_count + """ and title ilike %s """
 
+        if self.dp_from != '' and self.dp_to != '':
+            params.append(self.dp_from)
+            params.append(self.dp_to)
+            sql_count = sql_count + """ and %s <= modified_date and modified_date <= %s"""
+
+        logging.debug(sql_count)
+        self.app.cursor.execute(sql_count, tuple(params))
         count = self.app.cursor.fetchone()
         return count[0]
 
@@ -54,20 +63,31 @@ class JvnDAO(object):
                       where    a.identifier = b.identifier
                       and      b.cpe = c.cpe) as mail_query"""
  
-        sql_where = """where %s <= modified_date and modified_date <= %s"""
+        sql_where = """where true """
 
         sql_gr = """group by identifier, modified_date, issued_date
                     order by modified_date desc limit %s OFFSET %s;"""
 
-        if self.dp_from == '' :
-            self.app.cursor.execute(sql_main + ' ' + sql_gr, (PAGE_COUNT, offset * PAGE_COUNT,))
-        else:
-            self.app.cursor.execute(sql_main 
-                                    + ' ' + sql_where 
-                                    + ' ' + sql_gr, (self.dp_from, self.dp_to, PAGE_COUNT, offset * PAGE_COUNT,))
+        params = list()
+        if self.keyword != '' :
+            params.append(self.make_like(self.keyword))
+            sql_where = sql_where + """ and title ilike %s """
 
+        if self.dp_from != '' and self.dp_to != '':
+            params.append(self.dp_from)
+            params.append(self.dp_to)
+            sql_where = sql_where + """ and %s <= modified_date and modified_date <= %s """
+
+        params.append(PAGE_COUNT)
+        params.append(offset * PAGE_COUNT)
+
+        logging.debug(sql_main + ' ' + sql_where + ' ' + sql_gr)
+        self.app.cursor.execute(sql_main + ' ' + sql_where + ' ' + sql_gr, tuple(params))
         rows = self.app.cursor.fetchall()
         return rows
+
+    def make_like(self, word):
+        return word.replace(' ', '%') + '%'
 ################################################################################
 # セッションデータ
 ################################################################################
@@ -76,6 +96,7 @@ class JvnState(JvnPage):
         super(self.__class__, self).__init__()
         self.dp_from     = ''
         self.dp_to       = ''
+        self.keyword     = ''
 
 ################################################################################
 # 検索表示
@@ -101,6 +122,7 @@ class ListLogic(jvn_pagination.SearchModule):
             self.ui = session[self.pager_app] = JvnState()
 
         if 'dp_from' in req.params:
+            self.ui.keyword = req.params['keyword']
             self.ui.dp_from = req.params['dp_from']
             self.ui.dp_to   = req.params['dp_to']
 
