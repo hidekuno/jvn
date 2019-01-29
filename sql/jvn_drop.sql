67d6f75c59fb48c0b17303dc9ac62a10ac29727a
hidekuno <hidekuno@gmail.com>
2019-01-29 21:09:20 +0900

    sqlファイルを整理

---
+++ sql/jvn_drop.sql
@@ -0,0 +1,10 @@
+drop table if exists jvn_account;
+drop table if exists jvn_mainte_work;
+drop table if exists jvn_product;
+drop table if exists jvn_product_work;
+drop table if exists jvn_vendor;
+drop table if exists jvn_vendor_work;
+drop table if exists jvn_vulnerability;
+drop table if exists jvn_vulnerability_detail;
+drop table if exists jvn_vulnerability_detail_work;
+drop table if exists jvn_vulnerability_work;
