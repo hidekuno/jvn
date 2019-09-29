a43f0af059bb32bb579651da7842895a3ca0f5cf
hidekuno <hidekuno@gmail.com>
2019-09-29 20:21:04 +0900

    データベース名の変更

--- docker/database/jvn_init.sql
+++ docker/database/jvn_init.sql
@@ -1,2 +1,2 @@
-create role hideki with SUPERUSER LOGIN PASSWORD 'hideki';
-create database hideki_db WITH TEMPLATE = template0 OWNER = hideki encoding 'utf8' lc_collate 'ja_JP.utf8' lc_ctype 'ja_JP.utf8';
+create role jvn with SUPERUSER LOGIN PASSWORD 'jvn';
+create database jvn_db WITH TEMPLATE = template0 OWNER = jvn encoding 'utf8' lc_collate 'ja_JP.utf8' lc_ctype 'ja_JP.utf8';
