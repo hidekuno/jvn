85f3aba77eb4ec42713ce174c4dfd8d81304646b
hideki kuno <hidekuno@gmail.com>
2020-08-27 15:01:38 +0900

    dockerのentrypointシェルを修正

--- docker/web/docker-entrypoint.sh
+++ docker/web/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-chmod 777 /var/www/jvn/tmp /var/www/jvn/logs
+JVN_TMPDIR=/var/www/jvn/tmp
+JVN_LOGDIR=/var/www/jvn/logs
+
+su - www-data --shell=/bin/bash -c "test -w $JVN_TMPDIR && test -w $JVN_LOGDIR"
+
+if [ $? -eq 1 ]
+then
+    chmod 777 $JVN_TMPDIR $JVN_LOGDIR
+fi
 
 /usr/sbin/apache2ctl -D FOREGROUND
